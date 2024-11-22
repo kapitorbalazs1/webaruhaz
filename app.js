3fd6a26fb1d60eb9040b22f168223b56cdce201e
kapitorbalazs1 <kapitor.balazs.2020@oszalyterem.katolikuskeri.hu>
2024-11-22 13:05:39 +0100

    Adatbázis kapcsolat

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 const express = require('express');
-const fs = require('fs');
+const mysql = require('mysql');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req,res) =>{
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'webaruhaz'
+});
+
+db.connect(err => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySQL kapcsolat sikeres');
+});
+
+
+app.get('/', (req, res) => {
     res.status(200).json({
         status: 'siker',
-    })
-})
+    });
+});
+
+app.get('/felhasznalok', (req, res) => {
+    const sql = 'SELECT * FROM felhasznalok';
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ hiba: err.üzenet });
+        }
+        if (results.length === 0) {
+            return res.status(200).json({ üzenet: 'A tábla üres' });
+        }
+        res.status(200).json(results);
+    });
+});
 
 const port = 3000;
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`A szerver fut a ${port}-es porton`);
 });
