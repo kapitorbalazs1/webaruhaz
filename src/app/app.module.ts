24291d6af98c227349c3cb1f4a0f4ab6bb817378
kapitorbalazs1 <kapitor.balazs.2020@osztalyterem.katolikuskeri.hu>
2024-12-06 17:31:03 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
