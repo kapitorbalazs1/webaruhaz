3e900a32730a345157060208ec3bd4347079affc
kapitorbalazs1 <kapitor.balazs.2020@osztalyterem.katolikuskeri.hu>
2024-12-06 17:30:16 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,25 +5,4 @@ import { Component, HostListener } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  showFooter: boolean = false;
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    const windowHeight = window.innerHeight;
-    const documentHeight = document.documentElement.scrollHeight;
-
-    // Log the values for debugging
-    console.log(`Scroll Top: ${scrollTop}, Window Height: ${windowHeight}, Document Height: ${documentHeight}`);
-
-    // Check if the user has scrolled to the bottom of the page with a slight buffer
-    if (scrollTop + windowHeight >= documentHeight - 5) {
-      this.showFooter = true;
-      console.log('Footer should be shown');
-    } else {
-      this.showFooter = false;
-      console.log('Footer should be hidden');
-    }
-  }
-}
+export class AppComponent { }
